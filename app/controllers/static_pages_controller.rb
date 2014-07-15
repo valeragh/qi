f8b7d93becff2b4900dcb1b354da1a6c53971a38
valeragh <valeragh@gmail.com>
2014-07-15 17:59:15 +0300

    Add static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def faqs
+  end
+
+  def contact
+  end
+end
---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe StaticPagesController do
+
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'faqs'" do
+    it "returns http success" do
+      get 'faqs'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "returns http success" do
+      get 'contact'
+      response.should be_success
+    end
+  end
+
+end
